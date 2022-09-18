c8ccc850c74e3d3c2e3795110e69ecca753e5ad3
MinsuKim <minsu020302@gmail.com>
2022-09-19 05:07:11 +0900

    Feat : 파일 리더 추가

--- AISpeaker/text_to_speech.py
+++ AISpeaker/text_to_speech.py
@@ -6,13 +6,17 @@ from playsound import playsound
 
 file_name = 'sample.mp3'
 
+#text파일 열기
+with open("sample.txt", 'r', encoding='utf8') as f:
+  text = f.read()
+
 #ENG
 # text = 'can i help you?'
 # tts_en = gTTS(text = text, lang='en')
 # tts_en.save(file_name)
 
 #KOR
-text = '가나다라마바사'
+# text = '가나다라마바사'
 tts_ko = gTTS(text = text, lang='ko')
 tts_ko.save(file_name)
 
